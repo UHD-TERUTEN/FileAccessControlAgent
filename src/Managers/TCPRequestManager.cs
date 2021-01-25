08bcd8ee421d5192e7f8af056ea3faff83a7db6c
droplet92 <lhs1438@naver.com>
2021-01-25 10:08:21 +0900

    Manager 파일 생성

---
+++ src/Managers/TCPRequestManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+//using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FileAccessControlAgent.Managers
+{
+    interface ITCPRequest { }   // json
+    interface ITCPResponse { }  // json
+
+    static class TCPRequestManager
+    {
+        public static ITCPResponse SendRequest(this ITCPRequest request)
+        {
+            using (var pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.Out))
+            {
+                try
+                {
+                    using (var streamWriter = new StreamWriter(pipeServer))
+                    {
+                        //JsonSerializer.SerializeToUtf8Bytes();
+                        streamWriter.AutoFlush = true;
+                        streamWriter.WriteLine();
+                    }
+                }
+                catch (IOException e)
+                {
+                    ;
+                }
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
