b683ac594696006867e9150a8129181045145784
droplet92 <lhs1438@gmail.com>
2021-02-02 10:00:13 +0900

    시스템 트레이 사용

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,8 +1,15 @@
 ﻿using FileAccessControlAgent.ViewModels;
 using MahApps.Metro.Controls;
+using MahApps.Metro.IconPacks;
 using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
 using System.Windows.Automation.Peers;
 using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace FileAccessControlAgent
 {
@@ -18,6 +25,46 @@ namespace FileAccessControlAgent
             DataContext = new MainWindowViewModel(NavigateToInquiryMenu);
         }
 
+        private void mainWindowOnLoad(object sender, RoutedEventArgs e)
+        {
+            notifyIcon = new NotifyIcon()
+            {
+                Icon = Properties.Resources.icon,
+                Visible = true,
+                ContextMenuStrip = new ContextMenuStrip(),
+                Text = "파일 접근 관리 에이전트",
+            };
+
+            var openItem = new ToolStripMenuItem() { Text = "열기" };
+            var closeItem = new ToolStripMenuItem() { Text = "종료" };
+
+            notifyIcon.ContextMenuStrip.Items.Add(openItem);
+            notifyIcon.ContextMenuStrip.Items.Add(closeItem);
+
+            // Register event handlers
+            openItem.Click += ShowMainWindow;
+            closeItem.Click += (object sender, EventArgs e) =>
+            {
+                System.Windows.Application.Current.Shutdown();
+                notifyIcon.Dispose();
+            };
+            notifyIcon.DoubleClick += ShowMainWindow;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+            base.OnClosing(e);
+        }
+
+        private void ShowMainWindow(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = WindowState.Normal;
+            Visibility = Visibility.Visible;
+        }
+
         private void DoChangeMenuView(object parameter)
         {
             ((MainWindowViewModel)DataContext).ChangeMenuView.Invoke(parameter);
@@ -31,5 +78,7 @@ namespace FileAccessControlAgent
         }
 
         public Action<object> ChangeMenuView { get; set; }
+
+        private NotifyIcon notifyIcon;
     }
 }
